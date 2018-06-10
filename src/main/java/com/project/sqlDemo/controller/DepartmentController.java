897fd72b2cc36bc4e3385f105d3bca7824fab514
eztyhva <valentin.tyhonov@ericsson.com>
2018-06-10 16:45:45 +0300

    Added java docs

--- src/main/java/com/project/sqlDemo/controller/DepartmentController.java
+++ src/main/java/com/project/sqlDemo/controller/DepartmentController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/department")
 public interface DepartmentController {
 
+    /**
+     * This endpoint for getting all Department records from db
+     *
+     * @return JSON object with all Department records
+     */
     @RequestMapping(path = "", method = RequestMethod.GET)
     ResponseEntity<?> getAllDepartments();
 
--- src/main/java/com/project/sqlDemo/controller/EmployeeController.java
+++ src/main/java/com/project/sqlDemo/controller/EmployeeController.java
@@ -9,15 +9,45 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/employee")
 public interface EmployeeController {
 
+    /**
+     * This endpoint for getting page with Employee records from db
+     *
+     * @param pageable - object with information about
+     *                 requested page number and page size
+     * @return JSON object with Employee records and total number of pages
+     */
     @RequestMapping(path = "", method = RequestMethod.GET)
     ResponseEntity<?> getEmployees(Pageable pageable);
 
+    /**
+     * This endpoint for getting page with Employee records,
+     * which name starts with <name>, from db
+     *
+     * @param pageable - object with information about
+     *                 requested page number and page size
+     * @param name - string that record name should start with
+     * @return JSON object with Employee records,
+     *              which name starts with <name>,
+     *              and total number of pages
+     */
     @RequestMapping(path = "/search", method = RequestMethod.GET)
     ResponseEntity<?> searchEmployee(Pageable pageable, @RequestParam("name") String name);
 
+    /**
+     * This endpoint for putting updated Employee record to db
+     *
+     * @param employee - updated Employee record
+     * @return message with information of executing update operation
+     */
     @RequestMapping(path = "", method = RequestMethod.PUT)
     ResponseEntity<?> updateEmployee(@RequestBody Employee employee);
 
+    /**
+     * This endpoint for removing Employee record from db
+     *
+     * @param id - id of Employee record for removing from db
+     * @return message with information of executing delete operation
+     */
     @RequestMapping(path = "", method = RequestMethod.DELETE)
     ResponseEntity<?> removeEmployee(@RequestParam("id") long id);
 
