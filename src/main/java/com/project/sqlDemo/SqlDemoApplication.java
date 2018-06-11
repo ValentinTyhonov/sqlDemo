bc38d71b183c41332b2ac8d20cd895f9cf6d0be1
eztyhva <valentin.tyhonov@ericsson.com>
2018-06-11 10:12:37 +0300

    Fixes

--- src/main/java/com/project/sqlDemo/SqlDemoApplication.java
+++ src/main/java/com/project/sqlDemo/SqlDemoApplication.java
@@ -2,17 +2,9 @@ package com.project.sqlDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SqlDemoApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SqlDemoApplication.class);
-	}
-
+public class SqlDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SqlDemoApplication.class, args);
 	}
